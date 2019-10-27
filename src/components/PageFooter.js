3293e143dc1980ec9947c6c4a56c8c0477d1455a
Brittani Magee <brittgmagee@gmail.com>
2019-10-27 12:17:09 -0400

    Updated readme.md file

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -5,9 +5,6 @@ export default function PageFooter() {
     <div id="footer">
       <ul className="copyright">
         <li>&copy; 2019 Brittani Glynn Magee</li>
-        <li>
-          Gatsby Starter credited to: <a href="https://github.com/anubhavsrivastava/gatsby-starter-prologue">Anubhav Srivastava</a>
-        </li>
       </ul>
     </div>
   );
