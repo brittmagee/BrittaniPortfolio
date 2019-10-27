46b0a6a0846af41d24d743d6f31fc9286471235e
Brittani Magee <brittgmagee@gmail.com>
2019-10-27 11:48:31 -0400

    Updated 404.js call to Sidebar

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import SideBar from '../components/SideBar/Index';
+import SideBar from '../components/Sidebar';
 
 const IndexPage = () => (
   <Layout>
